c14097a6433566f588932f527cd65c6289c944d1
WonHyun Choi <coy4728@naver.com>
2024-07-15 13:39:51 +0900

    feat: add implicit animation

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,8 +9,59 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Implicit Animations"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.bounceOut,
+              width: size.width * 0.4,
+              height: size.width * 0.4,
+              transform: Matrix4.rotationZ(_visible ? 1 : 0),
+              transformAlignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: _visible ? Colors.red : Colors.amber,
+                borderRadius: BorderRadius.circular(_visible ? 100 : 0),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TweenAnimationBuilder(
+              tween: ColorTween(begin: Colors.blue, end: Colors.white),
+              duration: const Duration(milliseconds: 2000),
+              curve: Curves.bounceInOut,
+              builder: (context, value, child) {
+                return Image.network(
+                  "https://docs.flutter.dev/assets/images/dash/Dashatars.png",
+                  color: value,
+                  colorBlendMode: BlendMode.colorBurn,
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: const Text("Go!"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
