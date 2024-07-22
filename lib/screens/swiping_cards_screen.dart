8c664e1a19605939a541b69a60357778382222d4
WonHyun Choi <coy4728@naver.com>
2024-07-22 21:09:19 +0900

    feat: add swiping cards screen

---
+++ lib/screens/swiping_cards_screen.dart
@@ -0,0 +1,125 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SwipingCardsScreen extends StatefulWidget {
+  const SwipingCardsScreen({super.key});
+
+  @override
+  State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
+}
+
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.of(context).size;
+
+  late final AnimationController _position = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 1000),
+    lowerBound: (size.width + 100) * -1,
+    upperBound: (size.width + 100),
+    value: 0.0,
+  );
+
+  late final Tween<double> _rotaion = Tween(
+    begin: -15,
+    end: 15,
+  );
+
+  late final Tween<double> _scale = Tween(
+    begin: 0.8,
+    end: 1.0,
+  );
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {
+    _position.value += details.delta.dx;
+  }
+
+  void _onHorizontalDragEnd(DragEndDetails details) {
+    final bound = size.width - 200;
+    final dropZone = size.width + 100;
+
+    if (_position.value.abs() >= bound) {
+      if (_position.value.isNegative) {
+        _position.animateTo(dropZone * -1);
+      } else {
+        _position.animateTo(dropZone);
+      }
+    } else {
+      _position.animateTo(
+        0,
+        curve: Curves.easeOut,
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _position.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Swiping Cards"),
+      ),
+      body: AnimatedBuilder(
+        animation: _position,
+        builder: (context, child) {
+          final angle = _rotaion
+                  .transform((_position.value + size.width / 2) / size.width) *
+              pi /
+              180;
+          final scale = _scale.transform(_position.value.abs() / size.width);
+          return Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Positioned(
+                top: 100,
+                child: Transform.scale(
+                  scale: scale,
+                  child: Material(
+                    elevation: 10,
+                    color: Colors.blue.shade100,
+                    child: SizedBox(
+                      width: size.width * 0.8,
+                      height: size.height * 0.5,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                  onHorizontalDragEnd: _onHorizontalDragEnd,
+                  child: Transform.translate(
+                    offset: Offset(_position.value, 0),
+                    child: Transform.rotate(
+                      angle: angle,
+                      child: Material(
+                        elevation: 10,
+                        color: Colors.red.shade100,
+                        child: SizedBox(
+                          width: size.width * 0.8,
+                          height: size.height * 0.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
